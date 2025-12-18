470b659a029f02f7144b67524ce1f57ac735a037
fiza noor <fnoor034726@gmail.com>
2025-12-19 00:25:51 +0500

    images gathered

---
+++ src/components/Catogory.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Catogory = ({title,img,description}) => {
+  return (
+    <div>
+        <div>
+            <img src="" alt="" />
+        </div>
+    </div>
+  )
+}
+
+export default Catogory
